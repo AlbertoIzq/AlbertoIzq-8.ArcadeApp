c1da68414865ed3fd29a0700a6f80b7233322961
Alberto <alberto.izquierdomo@gmail.com>
2021-05-07 16:57:05 +0200

    Add and set up SDL2 library

---
+++ AlbertoIzq-8.ArcadeApp/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+#include "SDL.h"
+#undef main // To avoid error: external symbol _sdl_main referenced in function _main get cmdline
+
+int main(int argc, const char* argv[]) {
+	return 0;
+}
